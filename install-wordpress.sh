224210efd3eeb59e6ce4142bce28d48a582df21c
hbh <hbh@hbhdeMBP.lan>
2017-02-11 22:55:17 +0800

    安装WordPress

---
+++ install-wordpress.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+# Program:
+# 	安装 wordpress
+# 	2017年02月11日22:52:35 hbh
+PATH=/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-php5 php5
+sudo apt-get install MySQL-server-5.0  mysql-common mysql-admin
+sudo apt-get install php5-mysql
+wget -c http://wordpress.org/latest.tar.gz
+tar xvzf latest.tar.gz
+sudo cp -rf  wordpress /var/www
+sudo chown -R www-data:www-data wordpress
